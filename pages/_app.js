589ac8c68c7c5b3dfeeff19bbd61d2604936ab33
Chung Chang <chungchang@chungs-mbp.lan>
2022-08-26 22:07:12 -0700

    Connect to wallet using nextjs-web3react

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { web3ReactProvider } from "@web3-react/core";
+import { Web3Provider, web3Provider } from "@ethersproject/providers";
+
+const getLibrary = (provider) => {
+  return new Web3Provider(provider);
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Web3Provider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
